79ef0031a2746e722273c992eaf8f20fe906d7f5
LIBERCOSOFT <akinspreciousola@gmail.com>
2022-12-15 11:06:09 +0100

    Implement the list class and import MyEnumerable file

--- MyList.rb
+++ MyList.rb
@@ -1,10 +1,20 @@
+require_relative 'MyEnumerable'
+
 class MyList
+  include MyEnumerable
+
   def initialize(*args)
     @list = args
   end
 
-  def each; end
+  def each
+    counter = 0
+    while counter < @list.length
+      yield @list[counter]
+      counter += 1
+    end
+  end
 end
 
 newList = MyList.new(1, 2, 3, 4)
-puts newList
+newList.each { |num| puts num }
